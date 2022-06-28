0bef42ad47c1a17375c1b99118d8d745106994fd
Tyler Jackson <tylerjamesjackson@outlook.com>
2022-06-28 15:27:51 -0400

    add navbar layout

--- src/home.js
+++ src/home.js
@@ -1,8 +1,28 @@
 function home(){
     const content = document.getElementById('content');
+    
+    const navbar = document.createElement('div');
+    navbar.classList.add("navbar")
+    
     const heading = document.createElement('h1');
     heading.innerText = "Jackson's Kitchen";
-    content.append(heading);
+    heading.classList.add("heading")
+    navbar.append(heading);
+
+    
+    const home = document.createElement('h2');
+    home.innerText = "Home";
+    navbar.append(home);
+
+    const menu = document.createElement('h2');
+    menu.innerText = "Menu";
+    navbar.append(menu);
+
+    const contact = document.createElement('h2');
+    contact.innerText = "Contact";
+    navbar.append(contact);
+
+    content.append(navbar);
     document.body.style.backgroundImage = "url('/Users/tylerjackson/Desktop/Repos/Odin Project/restaurant-page/src/jason-leung-poI7DelFiVA-unsplash.jpg')";
 };
 
