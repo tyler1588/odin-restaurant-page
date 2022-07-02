83f4a323ba9d08aa36812b8088f729fe265f544a
Tyler Jackson <tylerjamesjackson@outlook.com>
2022-07-02 15:54:43 -0400

    add homepage content

--- src/home.js
+++ src/home.js
@@ -32,15 +32,34 @@ function home(){
 
     const intro = document.createElement('div');
     intro.classList.add("home-element", "intro");
+    const introHeading = document.createElement('h2');
+    introHeading.innerText = "Why Jackson's Kitchen?";
+    introHeading.classList.add("intro-heading");
+    intro.append(introHeading);
 
-    const about = document.createElement('div');
-    about.classList.add("home-element", "about");
+    const introBody = document.createElement('p');
+    introBody.innerText = "Jackson's has the best food! The atmosphere and customer service are luxurious! This is exactly the kind of place that I like to return to again and again.";
+    introBody.classList.add("intro-body");
+    intro.append(introBody);
+
+    homeSection.append(intro);
 
     const hours = document.createElement('div');
     hours.classList.add("home-element", "hours");
-    
-    homeSection.append(intro);
-    homeSection.append(about);
+
+    const hoursHeading = document.createElement('h2');
+    hoursHeading.innerText = "Hours";
+    hoursHeading.classList.add("hours-heading");
+    hours.append(hoursHeading);
+
+    var text = ["Monday: 10am - 10pm", "Tuesday: 10am - 10pm", "Wednesday 10am - 10pm", "Thursday 10am - 10pm", "Friday 10am - 2am", "Saturday 10am - 2am", "Sunday 12pm - 9pm"];
+    for(var i = 0; i < text.length; i += 1) {
+        var p = document.createElement("p");
+        p.className = "daily-hours";
+        p.innerHTML = text[i];
+        hours.append(p);
+    }
+
     homeSection.append(hours);
 
     content.append(homeSection);
