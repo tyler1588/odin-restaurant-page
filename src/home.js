e7879af68f0856776b6a3fdf32b0f1203b7be77b
Tyler Jackson <tylerjamesjackson@outlook.com>
2022-07-03 01:16:07 -0400

    refactor home.js to dynamically create DOM elements with a factory function

--- src/home.js
+++ src/home.js
@@ -1,3 +1,12 @@
+function createDomElement(type, innerText, classNames){
+    const DOMelement = document.createElement(type);
+    DOMelement.innerText = innerText;
+    for (let i = 0; i < classNames.length; i++){
+        DOMelement.classList.add(classNames[i]);
+    }
+    return DOMelement
+}
+
 function home(){
     document.body.style.backgroundImage = "url('/Users/tylerjackson/Desktop/Repos/Odin Project/restaurant-page/src/jazmin-quaynor-bm9zWKi_c9Y-unsplash.jpg')";
     const content = document.getElementById('content');
@@ -5,24 +14,16 @@ function home(){
     const navbar = document.createElement('div');
     navbar.classList.add("navbar")
     
-    const heading = document.createElement('h1');
-    heading.innerText = "Jackson's Kitchen";
-    heading.classList.add("heading");
+    const heading = createDomElement('h1', "Jackson's Kitchen", ['heading']);
     navbar.append(heading);
 
-    const home = document.createElement('h2');
-    home.innerText = "Home";
-    home.classList.add("selected-page", "home");
+    const home = createDomElement('h2', "Home", ["selected-page", "home"]);
     navbar.append(home);
 
-    const menu = document.createElement('h2');
-    menu.innerText = "Menu";
-    menu.classList.add("menu");
+    const menu = createDomElement('h2', "Menu", ["menu"]);
     navbar.append(menu);
 
-    const contact = document.createElement('h2');
-    contact.innerText = "Contact";
-    contact.classList.add("contact");
+    const contact = createDomElement('h2', "Contact", ["contact"]);
     navbar.append(contact);
 
     content.append(navbar);
@@ -32,14 +33,11 @@ function home(){
 
     const intro = document.createElement('div');
     intro.classList.add("home-element", "intro");
-    const introHeading = document.createElement('h2');
-    introHeading.innerText = "Why Jackson's Kitchen?";
-    introHeading.classList.add("intro-heading");
+
+    const introHeading = createDomElement('h2', "Why Jackson's Kitchen?", ["intro-heading"]);
     intro.append(introHeading);
 
-    const introBody = document.createElement('p');
-    introBody.innerText = "Jackson's has the best food! The atmosphere and customer service are luxurious! This is exactly the kind of place that I like to return to again and again.";
-    introBody.classList.add("intro-body");
+    const introBody = createDomElement('p', "Jackson's has the best food! The atmosphere and customer service are luxurious! This is exactly the kind of place that I like to return to again and again.", ["intro-body"]);
     intro.append(introBody);
 
     homeSection.append(intro);
@@ -47,24 +45,16 @@ function home(){
     const hours = document.createElement('div');
     hours.classList.add("home-element", "hours");
 
-    const hoursHeading = document.createElement('h2');
-    hoursHeading.innerText = "Hours";
-    hoursHeading.classList.add("hours-heading");
+    const hoursHeading = createDomElement('h2', "Hours", ["hours-heading"]);
     hours.append(hoursHeading);
 
-    var text = ["Monday: 10am - 10pm", "Tuesday: 10am - 10pm", "Wednesday 10am - 10pm", "Thursday 10am - 10pm", "Friday 10am - 2am", "Saturday 10am - 2am", "Sunday 12pm - 9pm"];
-    for(var i = 0; i < text.length; i += 1) {
-        var p = document.createElement("p");
-        p.className = "daily-hours";
-        p.innerHTML = text[i];
+    let text = ["Monday: 10am - 10pm", "Tuesday: 10am - 10pm", "Wednesday 10am - 10pm", "Thursday 10am - 10pm", "Friday 10am - 2am", "Saturday 10am - 2am", "Sunday 12pm - 9pm"];
+    for(let i = 0; i < text.length; i += 1) {
+        const p = createDomElement('p', text[i], ["daily-hours"]);
         hours.append(p);
     }
-
     homeSection.append(hours);
-
     content.append(homeSection);
-
-
 };
 
 export default home;
